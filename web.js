2b4595241d158c058f520e1c9f1fef764da3e35b
shawnnaquin <shawn.naquin@gmail.com>
2018-11-29 16:05:02 -0600

    working on cors

--- web.js
+++ web.js
@@ -36,8 +36,7 @@ http.createServer( function(request, response) {
     if(request.method == 'POST') {
         processPost(request, response, function() {
             console.log(request.post);
-            // Use request.post here
-            response.writeHead(200, "OK", {'Content-Type': 'text/plain'});
+            response.writeHead(200, response.post, {'Content-Type': 'text/plain'});
             response.end();
         });
     } else {
